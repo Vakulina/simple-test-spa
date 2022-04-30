c38c7dc4fca44827dbdb1bf3441c313147f616e3
Vakulina <viktoria.ganina@yandex.ru>
2022-04-30 20:13:10 +0300

    feat: add custom formInputs hook

--- src/components/ProfileForm/ProfileForm.tsx
+++ src/components/ProfileForm/ProfileForm.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IUser } from '../../types/types';
 import Button from "../Button/Button";
-import { useForm, IValues } from "../useForm/useForm";
+import { useFormsInputs, IValues } from "../useFormsInputs/useFormsInputs";
 import './ProfileForm.scss'
 
 interface ProfileFormProps {
@@ -9,11 +9,13 @@ interface ProfileFormProps {
   isDisabled: boolean;
 }
 
-
 const ProfileForm: React.FC<ProfileFormProps> = ({ user, isDisabled }) => {
+  const [errors, showErrors ] = React.useState<boolean>(false)
   /*  value?: string;
     type?: string;
     label?: string;
+
+
     error?: string;
     isValid?: boolean;
     required?: boolean;
@@ -26,53 +28,84 @@ const ProfileForm: React.FC<ProfileFormProps> = ({ user, isDisabled }) => {
       type: 'text',
       label: "Name",
       required: true,
+      validators: [
+        (s: string) => !s.length && 'Поле обязательно для заполнения',
+        (s: string) => s.length < 2 && 'Минимальная длина строки 2',
+        (s: string) => s.length > 60 && 'Мфксимальная длина строки 60',
+      ],
     },
     username: {
       value: `${(user && user.username)}`,
       type: 'text',
       label: "User name",
       required: true,
+      validators: [
+        (s: string) => s.length < 2 && 'Минимальная длина строки 2',
+        (s: string) => s.length > 60 && 'Максимальная длина строки 60',
+      ],
     },
     email: {
       value: `${user && user.email}`,
       type: 'text',
       label: "E-mail",
       required: true,
+      validators: [
+        (s: string) => !(/[^@\s]+@[^@\s]+\.[^@\s]+/).test(s)&& 'Введите валидный e-mail',
+      ],
     },
     street: {
       value: `${user && user.address.street}`,
       type: 'text',
       label: "Street",
       required: true,
+      validators: [
+        (s: string) => s.length < 2 && 'Минимальная длина строки 2',
+        (s: string) => s.length > 60 && 'Максимальная длина строки 60',
+      ],
     },
     city: {
       value: `${user && user.address.city}`,
       type: 'text',
       label: "City",
       required: true,
+      validators: [
+        (s: string) => s.length < 2 && 'Минимальная длина строки 2',
+        (s: string) => s.length > 60 && 'Максимальная длина строки 60',
+      ],
     },
     zipcode: {
       value: `${user && user.address.zipcode}`,
       type: 'text',
       label: "Zip code",
       required: true,
+      validators: [
+        (s: string) => !(/[1-9-]+/).test(s)&& 'Введите валидный zipcode',
+      ],
     },
     phone: {
       value: `${user && user.phone}`,
       type: 'text',
       label: "Phone",
       required: true,
+      validators: [
+        (s: string) => s.length < 2 && 'Минимальная длина строки 2',
+        (s: string) => s.length > 60 && 'Максимальная длина строки 60',
+      ],
     },
     website: {
       value: `${user && user.website}`,
       type: 'text',
       label: "Website",
       required: true,
+      validators: [
+        (s: string) => !(/^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/).test(s)&& 'Введите валидный website',
+      ],
+      
     },
 
   }
 
-  const { fields, isValid, handleSubmit } = useForm(formInputs);
+  const { fields, isValid } = useFormsInputs(formInputs);
 
   const { name, username, email, street, city, zipcode, phone, website } = fields;
 
@@ -81,96 +114,45 @@ const ProfileForm: React.FC<ProfileFormProps> = ({ user, isDisabled }) => {
   }
 
   const formFields = [name, username, email, street, city, zipcode, phone, website];
-  /*const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [username, setUserName] = React.useState<string|undefined>(user?user.username:undefined);
-
-
-
-/*const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);
-const [name, setName] = React.useState<string|undefined>(user?user.name:undefined);*/
-
-
-  /*      <form className='profile__form' noValidate >
-          <fieldset disabled={isDisabled}>
-              <label
-                className='profile__label'
-                htmlFor='name'>Name</label>
-              <input
-                className='profile__input'
-                name='name'
-                type="text"
-                minLength={2}
-                maxLength={30}
-                pattern='[A-Za-zа-яА-Я -_.]+'
-                required
-                id='name'
-       
-                 />
   
-                            <label
-                className='profile__label'
-                htmlFor='user'>User</label>
-              <input
-                className='profile__input'
-                name='user'
-                type="text"
-                minLength={2}
-                maxLength={30}
-                pattern='[A-Za-zа-яА-Я -_.]+'
-                required
-                id='user'
-    
-                 />
-             
-          
-             </fieldset>
-             <Button onClick={()=>{handleSubmit(onSubmit)}}  buttonText="Отправить" className="button_submit-form"/>
-        </form>*/
   return (
-
     <>
-      <form className='profile__form' onSubmit={handleSubmit(onSubmit)}>
+      <form className='profile__form' onSubmit={(e) => {
+        e.preventDefault()
 
+        if (isValid) {
+          console.log(fields)
+        }
+        else {
+          showErrors(true)
+        }
+      }}>
         <fieldset disabled={isDisabled}>
-
           {formFields.map((field, index) => (
-
-       
-              <label key={index}
-                className='profile__label'
-              >
+            <>
+              <label key={index} className='profile__label'>
                 {field.label}
                 <input
                   type='text'
-
+                  className={!field.isValid ? 'profile__input_invalid' : ''}
                   value={field.value}
-
+                  onChange={field.setState}
                 />
               </label>
-
+              {errors&&field.error && <span className="profile__error">{
+                field.error
+              }</span>}
+            </>
           )
-
           )}
           <label className='profile__label'>
             Comment
             <textarea className="profile__comment"></textarea>
           </label>
-   
- 
-
         </fieldset>
-        <Button className="button_send-form" onClick={()=>{}} buttonText="Отправить" disabled={isDisabled}/>
+        <Button className="button_send-form" onClick={() => { }} buttonText="Отправить" disabled={isDisabled} />
       </form>
-
-
-
     </>
-
   )
 }
 export default ProfileForm;
---
+++ src/components/useFormsInputs/useFormsInputs.tsx
@@ -0,0 +1,116 @@
+import { ChangeEvent,  useState } from 'react';
+
+type IValidatorFN = (s: string) => {};
+
+export interface IField {
+  value?: string;
+  type?: string;
+  label?: string;
+  error?: string;
+  isValid?: boolean;
+  required?: boolean;
+  setState?: (event: ChangeEvent<HTMLInputElement>) => {};
+  validators?: IValidatorFN[];
+}
+
+export type ICustomField<T = {}> = IField & T;
+
+export type ICustomObject<T = {}> = {
+  [key: string]: ICustomField & T;
+}
+
+export type IValues = {
+  [key: string]: string;
+}
+
+export type IForm = {
+  fields: ICustomObject;
+  isValid: boolean;
+ 
+}
+
+type IOptions = {
+  [key: string]: string;
+}
+
+export const useFormsInputs = (initialFields: ICustomObject): IForm => {
+  const form = Object.entries(initialFields).reduce((fields, [name, value]: any[]) => {
+    const isString = typeof value === 'string';
+
+    const field = {
+      [name]: {
+        type: 'text',
+        value: (isString && value) || ((!isString && value.value) || ''),
+        error: (!isString && value.error) || null,
+        validators: (!isString && value.validators) || null,
+        isValid: (!isString && value.isValid) || true,
+        required: (!isString && value.required) || false,
+          setState: (value: ChangeEvent<HTMLInputElement>) => handleInput(value, name),
+        ...(!isString && value),
+      },
+    };
+
+    return {...fields, ...field};
+  }, {});
+
+  const [fields, setState] = useState<ICustomObject>(form);
+  const [isValid, setFormValid] = useState<boolean>(true);
+
+  const getFormValidationState = (fields: ICustomObject): boolean =>
+    Object.entries(fields).reduce((isValid: boolean, [_, value]: any) => Boolean(Number(isValid) * Number(value.isValid)), true);
+
+
+  const fieldValidation = (field: ICustomField, options: IOptions = {}) => {
+    const { value, required, validators } = field;
+
+    let isValid = true, error;
+
+
+    if (required) {
+      isValid = !!value;
+    }
+
+    if (validators && Array.isArray(validators)) {
+      const results = validators.map(validateFn => {
+        if (typeof validateFn === 'string') return validateFn;
+    
+        const validationResult = validateFn(value || '');
+    
+        return typeof validationResult === 'string' ? validationResult : '';
+      }).filter(message => message !== '');
+      
+      if (results.length) {
+        isValid = false;
+        error = results[0];
+      }
+    }
+
+    return { ...field, isValid, error, ...options };
+  };
+
+  const handleInput = (element: ChangeEvent<HTMLInputElement>, name: string) => {
+    const input = fields[name];
+    const value = element.target.value;
+
+    const field = {
+      ...input,
+      value,
+      isValid: true,
+    };
+
+    const validatedField = fieldValidation(field);
+
+    setState((prevState: ICustomObject) => {
+      const items = {...prevState, [name]: validatedField};
+
+      setFormValid(getFormValidationState(items));
+      return items;
+    });
+  }
+
+
+  return {
+    fields,
+    isValid
+  }
+}
